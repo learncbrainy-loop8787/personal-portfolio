6bb6e173516fc4650a6f36045e67b20a1ce83bed
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-07-04 21:02:18 -0700

    password reset view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', controllers: { registrations: 'users/registrations', sessions: 'users/sessions' },
+  devise_for :users, path: '', controllers: { registrations: 'users/registrations', sessions: 'users/sessions', passwords: 'users/passwords' },
              path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: '' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
