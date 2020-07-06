3731c246b26a93c4b4174d3bc6ad4b28f443d30d
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-07-06 16:38:00 -0700

    apikey added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -113,6 +113,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
+    api_key: ENV['SENDGRID_API_KEY'],
     domain: 'oscardelalanza-portfolio.herokuapp.com',
     address: 'smtp.sendgrid.net',
     port: 587,
