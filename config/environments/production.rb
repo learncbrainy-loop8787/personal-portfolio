824753238dd3159e30dd0405537bb94f66d4d95a
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-07-06 16:30:46 -0700

    actionmeiler setup:

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,13 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'oscardelalanza-portfolio.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
