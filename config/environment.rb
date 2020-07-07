e1b8dc2e5f69b53dae2247ed7606f0f321d46ff3
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-07-06 17:12:44 -0700

    config

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,6 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
   domain: 'oscardelalanza-portfolio.herokuapp.com',
   address: 'smtp.sendgrid.net',
   port: 587,
